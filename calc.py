d841dcd26057cecc25a36a43469fbc7ab34f0d06
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-10-11 18:01:04 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,18 +1,11 @@
-
-
-def minus(num1,num2):
+def calc(num1, operator, num2):
     num1 = int(num1)
     num2 = int(num2)
-    return num1 - num2
-def plus(num1,num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return num1 + num2
-def multiplication(num1,num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return num1 * num2
-def division(num1,num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    return num1 / num2
+    if operator == '-':
+        return num1 - num2
+    elif operator == '+':
+        return num1 + num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
