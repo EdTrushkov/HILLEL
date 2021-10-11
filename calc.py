cf4923f91e98b0c1a57122c7850fe925c8d56687
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-10-11 15:40:38 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,18 @@
+
+
+def minus(num1,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 - num2
+def plus(num1,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 + num2
+def multiplication(num1,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 * num2
+def division(num1,num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    return num1 / num2
