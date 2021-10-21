5d92d1e3203b4810d96832ecc250eddb40c1908e
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-10-21 17:01:52 +0300

    Add files via upload

---
+++ HW11.py
@@ -0,0 +1,16 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def to_polar(self):
+        return [math.atan2(y, x), math.sqrt(x ** 2 + y ** 2)]
+
+
+x = int(input("Print first dekart coordinate: "))
+y = int(input("Print second dekart coordinate: "))
+print(f"Polar coordinate {Point.to_polar(Point(x, y))}")
+
