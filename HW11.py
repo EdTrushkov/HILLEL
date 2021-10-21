dc2546043ad798998dcceeeb4759ed40199fadab
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-10-21 17:12:33 +0300

    Update HW11.py

--- HW11.py
+++ HW11.py
@@ -12,5 +12,6 @@ class Point:
 
 x = int(input("Print first dekart coordinate: "))
 y = int(input("Print second dekart coordinate: "))
-print(f"Polar coordinate {Point.to_polar(Point(x, y))}")
+point = Point(x, y)
+print(f"Polar coordinate {Point.to_polar(point)}")
 
