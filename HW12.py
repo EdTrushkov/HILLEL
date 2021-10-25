84a5f6f21bf8480bfe1f44ab78e313d495ec189f
EdTrushkov <90473867+EdTrushkov@users.noreply.github.com>
2021-10-25 16:16:45 +0300

    Update HW12.py

--- HW12.py
+++ HW12.py
@@ -1,19 +1,19 @@
-
-
 class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def minus(self, vector):
-        self.x -= vector.x
-        self.y -= vector.y
+    def __sub__(self, other, y = 0):
+        if isinstance(other, Vector):
+            return self.x - other.x, self.y - other.y
+        if isinstance((other, y), (int, float)):
+            return self.x - other, self.y - y
 
-    def plus(self, vector):
-        self.x += vector.x
-        self.y += vector.y
+    def __add__(self, other, y = 0):
+        if isinstance(other, Vector):
+            return self.x + other.x, self.y + other.y
+        if isinstance((other, y), (int, float)):
+            return self.x + other, self.y + y
 
-    def multiplication(self, b):
-        self.x *= b
-        self.y *= b
-
+    def __mul__(self, b):
+        return self.x * b, self.y * b
